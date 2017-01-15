62bc253034f0f50080558377394a3a1cbb68c4c7
JJTB Somhorst <info@jeroensomhorst.eu>
2017-01-15 13:46:29 +0100

    fixed a bug that would crash the app

--- libs/flowProcessor.js
+++ libs/flowProcessor.js
@@ -15,48 +15,43 @@ class flowProcessor{
         this.speechoutput = Homey.manager('speech-output');
         this.flowManager = Homey.manager('flow');
         this.flowManager.on(ACTION_READ_FEED,this.onReadRssFeed.bind(this));
-
-        
-        
     }
 
     onReadRssFeed(cb,args){
-        if(this.parser == null){
-             this.parser = new FeedMe(true);
-        }
-
-        this.parser.on('end',this.onFeedParsed.bind(this,cb,args));
+        var parser = new FeedMe(true);
+        
+        parser.on('end',this.onFeedParsed.bind(this,cb,args,parser));
        http.get(args.url,function(res){
-           res.pipe(this.parser);
+           res.pipe(parser);
        }.bind(this));
     }
 
-    onFeedParsed(cb,args){
+    onFeedParsed(cb,args,parser){
         Homey.log(cb);
         Homey.log(args);
-        var feedContent = this.parser.done();
-        var items = feedContent.items;
+        try{
+            var feedContent = parser.done();
+            var items = feedContent.items;
 
-        if(items.length > args.articlecount){
+            if(items.length > args.articlecount){
             items = items.slice(0,args.articlecount);
-        }
-        var itemlenght = items.length;
+            }
 
-        for(var i = 0; i < itemlenght;i++){
+            var itemlenght = items.length;
+
+            for(var i = 0; i < itemlenght;i++){
             var item = items[i];
-            
+
             this.readEntry(null,item,i,cb,args);
 
-
-            if(i == itemlenght-1){
-                items = null;
-                feedContent = null;
-                item = null;
             }
+
+            cb(null,true);
+            
+        }catch(err){
+            Homey.log('error');
         }
-       
-       cb(null,true);
-       this.parser = null;
+        
     }
 
     readEntry(collection, item, index, callback, args){
